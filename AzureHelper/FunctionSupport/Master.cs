56b15de753de7e5363c31e524f5feca3793ae4ec
JuanPablo <jpgarciaazure@hotmail.com>
2017-03-21 13:24:46 -0400

    Original Commit

---
+++ AzureHelper/FunctionSupport/Master.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace FunctionSupport
+{
+    public class Master
+    {
+        JObject myTemplate;
+        public Master(string jsonTemplate)
+        {
+            myTemplate = JObject.Parse(jsonTemplate);
+        }
+
+
+        public string GetDefaulValue(string paramName)
+        {
+            string value = "";
+            try
+            {
+                value = myTemplate["parameters"][paramName]["defaultValue"].ToString();
+            }
+            catch (Exception X)
+            {
+                Trace.TraceError(X.Message);
+            }
+            return value;
+        }
+
+        public string GetAllowedValues(string paramName)
+        {
+            string values = "";
+            try
+            {
+                values= myTemplate["parameters"][paramName]["allowedValues"].ToString();
+            }
+            catch (Exception X)
+            {
+                Trace.TraceError(X.Message);
+
+            }
+            return values;
+        }
+    }
+}
